bf6dedd252d8160cf5e73b3a2002b34ab90c4be2
ryan anzer <ryan.anzer@epitech.eu>
2018-02-02 01:47:22 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = function() {
+	return ({
+		ignore: [],
+		dependencies: {},
+		route: [
+			'config/config.js'
+		],
+		cdn: [],
+		import: []
+	});
+};
---
+++ entity/entity.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function($) {
+    return $.require([
+        //
+    ], function(
+        //
+    ) {
+
+        var obj = function() {};
+        obj.prototype = {
+            //
+        };
+
+        return ({'private': obj});
+    });
+};
